ae582ae40e132c9fd253926a24cc78b9083154c6
srchou <srchou@naver.com>
2022-11-07 17:53:46 +0900

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.vue 	modified:   src/router/index.ts 	new file:   src/views/LoginView.vue

--- dphoto/src/App.vue
+++ dphoto/src/App.vue
@@ -1,30 +1,24 @@
-<template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
-</template>
+<v-app>
+  <v-navigation-drawer app>
+    <!-- -->
+  </v-navigation-drawer>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
 
-nav {
-  padding: 30px;
+  <!-- Sizes your content based upon application components -->
+  <v-main>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
